f9cb815b3e6fc1cc041294c50db612fdcc1a7d57
Yiays <yesiateyoursheep@gmail.com>
2023-06-10 23:18:33 +1200

    Added landing page for app links

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<?php
+$title = "PukekoHost for smart devices";
+$subtitle = "PukekoHost App";
+require_once("../includes/header.php");
+?>
+<div class="wrapper main">
+	<h1>The PukekoHost App Provides a native experience for managing your servers</h1>
+	<p>Install the PukekoHost app from the Play Store or Microsoft Store to continue, or, <a href="/">just use the website</a>.</p>
+</div>
+<?php
+require_once("../includes/footer.php");
+?>
