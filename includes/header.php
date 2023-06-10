6675491befb170279394214028c8d9b0fab37307
Yiays <yesiateyoursheep@gmail.com>
2023-06-10 23:18:00 +1200

    New device theme respecting favicon

--- includes/header.php
+++ includes/header.php
@@ -37,6 +37,7 @@ $account->conn = $conn;
 	<meta name="author" content="Yiays and Дункан">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg">
 	<link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
