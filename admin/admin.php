3276675f880dadf10b59565b57338b22b043e7fe
Yiays <yesiateyoursheep@gmail.com>
2024-11-23 22:46:50 +1300

    Update to JQuery 3.7.1, source locally

--- admin/admin.php
+++ admin/admin.php
@@ -66,8 +66,7 @@ $title = "Admin";
 	</div>
 	
 	<script src="/js/vendor/modernizr-3.8.0.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
+	<script src="https://cdn.yiays.com/jquery-3.7.1.min.js"></script>
   <script src="/js/plugins.js"></script>
   <script src="/js/main.js?v=5"></script>
 </body>
